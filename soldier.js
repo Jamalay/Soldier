cb6ca3c695a1d28076d68ce1161bc148b00b6ef0
Jamalay <dzamaevmansur12@gmail.com>
2023-06-27 15:09:59 +0300

    Добавлен объект soldier с информацией о персонаже и методами Shoot, Reload, toWount

---
+++ soldier.js
@@ -0,0 +1,18 @@
+const soldier = {
+    name: 'J',
+    health: 10,
+    weapon: {
+        weapon_name: 'Калаш',
+        bullets: 30,
+    },
+    stores: 3,
+    Shoot: function(){
+
+    },
+    Reload: function(){
+
+    },
+    toWount: function(){
+
+    }
+}
