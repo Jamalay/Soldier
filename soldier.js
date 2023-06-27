a564497db2517eb46cec9d6af3582c8138d2a772
Jamalay <dzamaevmansur12@gmail.com>
2023-06-27 15:23:25 +0300

    в методы добавлен функционал

--- soldier.js
+++ soldier.js
@@ -7,12 +7,32 @@ const soldier = {
     },
     stores: 3,
     Shoot: function(){
-
+        if (this.weapon.bullets <= 0){
+            console.log('Обойма пуста. Перезаредитесь')
+        }
+        else{
+            this.weapon.bullets--;
+            console.log('бах-бах');
+        }
     },
     Reload: function(){
-
+        if (this.stores <= 0){
+            console.log('не осталось припасов.');
+        }
+        else{
+            this.weapon.bullets = 30;
+            stores--;
+            console.log('перезарядка')
+        }
     },
     toWount: function(){
-
+        if (this.health <= 0){
+            console.log('Ты проиграл');
+        }
+        else{
+            this.health--;
+        }
     }
 }
+
+soldier.Shoot();
